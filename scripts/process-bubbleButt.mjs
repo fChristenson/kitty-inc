2fd93ce61e41dd33c915c6f0919e0dfe84dc628a
Fredrik Christenson <fredrik.christenson@ericsson.com>
2026-09-23 17:51:28 +0200

    qq

--- scripts/process-bubbleButt.mjs
+++ scripts/process-bubbleButt.mjs
@@ -3,12 +3,21 @@ import { processCritIcon } from "./lib/process-crit-icon.mjs";
 
 await processCritIcon("bubbleButt", {
   sourceExtension: ".png",
-  backgroundSeeds: [[800, 1000], [715, 510]],
+  backgroundSeeds: [
+    [800, 1000],
+    [715, 510],
+  ],
 });
-await fs.mkdir(new URL("../src/assets/themes/references/dist/", import.meta.url), {
-  recursive: true,
-});
+await fs.mkdir(
+  new URL("../src/assets/themes/references/dist/", import.meta.url),
+  {
+    recursive: true,
+  },
+);
 await fs.copyFile(
   new URL("../public/bubbleButt.png", import.meta.url),
-  new URL("../src/assets/themes/references/dist/bubbleButt.png", import.meta.url),
+  new URL(
+    "../src/assets/themes/references/dist/bubbleButt.png",
+    import.meta.url,
+  ),
 );
--- scripts/process-canNotLie.mjs
+++ scripts/process-canNotLie.mjs
@@ -5,10 +5,16 @@ await processCritIcon("canNotLie", {
   sourceExtension: ".png",
   backgroundSeeds: [[800, 1000]],
 });
-await fs.mkdir(new URL("../src/assets/themes/references/dist/", import.meta.url), {
-  recursive: true,
-});
+await fs.mkdir(
+  new URL("../src/assets/themes/references/dist/", import.meta.url),
+  {
+    recursive: true,
+  },
+);
 await fs.copyFile(
   new URL("../public/canNotLie.png", import.meta.url),
-  new URL("../src/assets/themes/references/dist/canNotLie.png", import.meta.url),
+  new URL(
+    "../src/assets/themes/references/dist/canNotLie.png",
+    import.meta.url,
+  ),
 );
