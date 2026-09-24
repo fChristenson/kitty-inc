de4cd7b00275d7b555f841857782f0fb928b5476
Fredrik Christenson <fredrik.christenson@ericsson.com>
2026-09-24 14:18:07 +0200

    kk

---
+++ scripts/process-capitalCarousel.mjs
@@ -0,0 +1,9 @@
+import { processCritIcon } from "./lib/process-crit-icon.mjs";
+
+await processCritIcon("capitalCarousel", {
+  backgroundSeeds: [
+    [502, 427],
+    [712, 427],
+  ],
+  copyToAssetDirectories: true,
+});
